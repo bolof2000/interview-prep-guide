805848ca48e914047465856725b5730a7304ab13
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-05-15 22:35:35 +0000

    spiral matrix

--- leetcode/questions.py
+++ leetcode/questions.py
@@ -1096,8 +1096,43 @@ class MSTInterviewPrepSolutions(object):
     return result
 
 
-  def spiralMatrix(self,grid):
-    
+  def spiralMatrix(self,matrix):
+
+    top = 0
+    bottom = len(matrix)-1
+    left =0
+    right = len(matrix[0])-1
+    direction = "right"
+    result = []
+
+    while top <= bottom and left <= right:
+      if direction == "right":
+        for i in range(right,left+1):
+          result.append(matrix[top][i])
+        top +=1
+        direction = "down"
+      elif direction = "down":
+        for i in range(top,bottom-1,-1):
+          result.append(matrix[right][i])
+        right -=1
+        direction = "left"
+      elif direction == "left":
+        for i in range(left,right-1,-1):
+          result.append(matrix[bottom][i])
+        bottom -=1
+        direction = "up"
+      
+      elif direction == "up":
+        for i in range(bottom,top-1,-1):
+          result.append(matrix[left][i])
+        left +=1
+        direction = "right"
+
+    return result 
+
+
+
+
 
 
     
